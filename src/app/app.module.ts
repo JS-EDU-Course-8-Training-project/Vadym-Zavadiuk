107c7aceec29d0f6d4fd4c2be4f5b0bb2bd20cde
Vadym Zavadiuk - Dev.Pro <vadym.zavadiuk@dev.pro>
2022-04-15 15:59:07 +0300

    JEC8-104 create authorization component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
 import { HomeModule } from './home/home.module';
 import { HeaderComponent, SharedModule } from './shared';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, SharedModule, HomeModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    SharedModule,
+    HomeModule,
+    AppRoutingModule,
+    AuthModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
