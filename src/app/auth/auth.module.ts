854dddd4ccc1d4f25e99c69058da14e6ac5da2b0
Vadym Zavadiuk - Dev.Pro <vadym.zavadiuk@dev.pro>
2022-04-21 12:42:00 +0300

    JEC8-135 deny auth users link to register or login

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth.component';
 import { SharedModule } from '../shared';
 import { AuthRoutingModule } from './auth-routing.module';
+import { NoAuthGuard } from './no-auth-guard.service';
 
 @NgModule({
   declarations: [AuthComponent],
   imports: [CommonModule, SharedModule, AuthRoutingModule],
+  providers: [NoAuthGuard],
 })
 export class AuthModule {}
--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,3 +1,3 @@
-export * from './layout';
+export * from './layout/header.component';
 export * from './shared.module';
 export * from './show-authed.directive';
